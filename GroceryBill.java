a34260b5466e51fb8b5b112389d51204812cd2d6
Arindam Baidya <imarindambaidya.com>
2024-12-20 13:55:27 +0530

    Added 18% GST

--- GroceryBill.java
+++ GroceryBill.java
@@ -12,8 +12,11 @@ public class GroceryBill{
         System.out.println("Cost of pencil is : " + pencil + " , Cost of Pen is : " + pen +" , and Cost of eraser is : " + ereser);
 
         float totalPrice = pencil + pen + ereser;
-
         System.out.println("Total Cost is " + totalPrice);
+
+        System.out.println("Adding 18% GST on Total Price");
+        float finalCost = (totalPrice * 0.18f);
+        System.out.println("Final Cost is : " + finalCost);
 
         sc.close();
     }
